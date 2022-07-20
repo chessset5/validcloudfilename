d48271ff2b78422f4e27ccb9fd43406014951a8c
ashackelford <ashackelford@csus.edu>
2022-07-20 16:47:18 -0700

    update Jul 20 4:47 pm PST

---
+++ test_filename.py
@@ -0,0 +1,9 @@
+import unittest
+import filename
+
+class Test_stringClean(unittest.TestCase):
+    def test_badstrings(self):
+        self.assertEqual(filename.stringClean("f//alamf3fw0"),"f  alamf3fw0")
+
+if __name__ == '__main__':
+    unittest.main()
